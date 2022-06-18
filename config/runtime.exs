89070bd710495eae6e106c148d15142999132a25
Roberto Estrada <restrada@paridin.com>
2022-06-18 02:08:20 -0500

    check origin flag

--- config/runtime.exs
+++ config/runtime.exs
@@ -61,6 +61,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
+    check_origin: (System.get_env("HTTP_ORIGINS") || "localhost") |> String.split(" "),
     secret_key_base: secret_key_base
 
   # ## Configuring the mailer
